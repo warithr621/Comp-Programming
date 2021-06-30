291dd3e8f90c306323247c7800b2203a26fafebb
Warith Rahman <64328893+warithr621@users.noreply.github.com>
2021-06-30 13:27:10 -0500

    Create meal_trip.go

---
+++ CodeForces/meal_trip.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func min(a, b int) int {
+    if a < b {
+        return a;
+    }
+    return b;
+}
+
+func main() {
+    var n int
+    var a int
+    var b int
+    var c int
+    fmt.Scanln(&n); fmt.Scanln(&a); fmt.Scanln(&b); fmt.Scanln(&c);
+    ans := min(a, b);
+    if (n == 1 || !(c < a && c < b)) {
+        fmt.Println(ans * (n-1));
+    } else {
+        fmt.Println(ans + c * (n-2));
+    }
+}
