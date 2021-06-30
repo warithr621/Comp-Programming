eecd4fa163b06f678c93336888bf05cb33d6704d
Warith Rahman <64328893+warithr621@users.noreply.github.com>
2021-06-30 14:14:17 -0500

    Update playoff.py

--- CodeForces/playoff.py
+++ CodeForces/playoff.py
@@ -1,3 +1,5 @@
+#Problem: https://codeforces.com/contest/1535/problem/A
+
 for _ in range(int(input())):
     a,b,c,d = map(int, input().split())
     if max(a,b) > min(c,d) and max(c,d) > min(a,b):
