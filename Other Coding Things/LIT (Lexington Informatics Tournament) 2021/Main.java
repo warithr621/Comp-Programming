099d06140bebc2e881dbe23e18cb222668b8951c
Warith Rahman <64328893+warithr621@users.noreply.github.com>
2021-07-03 10:31:54 -0500

    Add files via upload

---
+++ Other Coding Things/LIT (Lexington Informatics Tournament) 2021/Main.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    static StringTokenizer st = new StringTokenizer("");
+    static int T = 1;
+    public static void main(String args[]) throws IOException{
+    // 	T = Integer.parseInt(next());
+        for (int t = 1; t <= T; t++) {
+            int n = Integer.parseInt(next()), m = Integer.parseInt(next()), l = Integer.parseInt(next());
+            int lim = 0, ans = 0;
+            Queue<Integer> q = new PriorityQueue<Integer>();
+            while(n-->0) {
+                int inp = Integer.parseInt(next());
+                int end = -1;
+                while(q.size() > 0 && q.peek() < inp) {
+                    --lim;
+                    end = q.poll();
+                }
+                if (end != -1 && lim == 0) {
+                    ans += inp - end - 1;
+                }
+                if (lim == l) {
+                    q.offer(q.peek() + m);
+                    q.poll();
+                }
+                if (lim < l) {
+                    q.offer(inp + m - 1);
+                    ++lim;
+                }
+            }
+            out.println(ans);
+        }
+        br.close(); out.close();
+    }
+    static String next() throws IOException{
+        while(!st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+        return st.nextToken();
+    }
+}
