af805842fde0fba502a28c8c9fedb2b8a74e86e4
Warith Rahman <64328893+warithr621@users.noreply.github.com>
2021-07-18 18:17:21 -0500

    Create bin_dec.java

---
+++ CodeForces/bin_dec.java
@@ -0,0 +1,28 @@
+//Problem: https://codeforces.com/contest/1530/problem/A
+
+import java.io.*;
+import java.util.*;
+
+public class bin_dec {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    static StringTokenizer st = new StringTokenizer("");
+    static int T = 1;
+    public static void main(String args[]) throws IOException{
+    	T = Integer.parseInt(next());
+        for (int t = 1; t <= T; t++) {
+            char[] c = next().toCharArray();
+            Arrays.sort(c);
+            out.println(c[c.length-1]);
+        }
+        br.close(); out.close();
+    }
+    static String next() throws IOException{
+        while(!st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+        return st.nextToken();
+    }
+    static int next(int x) throws IOException {
+        while(!st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+        return Integer.parseInt(st.nextToken());
+    }
+}
