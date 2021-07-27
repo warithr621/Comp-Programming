766b2b878540556143a1b02f4e3df3d1d6e38062
Warith Rahman <64328893+warithr621@users.noreply.github.com>
2021-07-27 15:04:46 -0500

    Update Baldo.java

--- UIL Computer Science/2017-2018/Invitational B/Baldo.java
+++ UIL Computer Science/2017-2018/Invitational B/Baldo.java
@@ -15,8 +15,4 @@ public class Baldo {
         }
         br.close(); out.close();
     }
-    public static int gcd(int a, int b) {
-        if (b == 0) return a;
-        return gcd(b, a % b);
-    }
 }
