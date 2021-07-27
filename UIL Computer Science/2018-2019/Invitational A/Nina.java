f6daca2426d40f3e4afb08f11450b159e67b5294
Warith Rahman <64328893+warithr621@users.noreply.github.com>
2021-07-27 16:21:35 -0500

    Create Nina.java

---
+++ UIL Computer Science/2018-2019/Invitational A/Nina.java
@@ -0,0 +1,28 @@
+//Problem 9
+
+import java.io.*;
+import java.util.*;
+
+public class Nina {
+    static int T = 1;
+    public static void main(String args[]) throws IOException{
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new FileReader("nina.dat"));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+        T = Integer.parseInt(br.readLine());
+        for (int ttt = 1; ttt <= T; ttt++) {
+            double r = Double.parseDouble(br.readLine());
+            double pi = Math.PI;
+            double side = 2 * r * Math.sin(pi / 5);
+            double area = (5 * side * side) / (4 * Math.tan(pi/5));
+            out.printf("LOCATION #%d ", ttt);
+            if (area > 43560) out.println("WILL NOT FIT");
+            else out.printf("%.2f %.2f%n", area, 5 * side);
+        }
+        br.close(); out.close();
+    }
+    static long gcd(long a, long b) {
+        if (b == 0) return a;
+        return gcd(b, a%b);
+    }
+}
