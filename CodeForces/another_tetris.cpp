eebe37e8167f2ad62c7b0bdefb09b1bfe1419bb4
Warith Rahman <64328893+warithr621@users.noreply.github.com>
2021-07-27 12:35:47 -0500

    Add files via upload

---
+++ CodeForces/another_tetris.cpp
@@ -0,0 +1,48 @@
+//Problem: https://codeforces.com/contest/1324/problem/A
+//Note: Virtual Submission
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define double long double;
+typedef vector<int> vi;
+typedef map<int, int> mii;
+typedef pair<int, int> pii;
+#define s(v) sort(v.begin(), v.end())
+#define rev(v) sort(v.rbegin(), v.rend())
+#define bs(v, s) binary_search(v.begin(), v.end(), s)
+#define minvec(v) *min_element(v.begin(), v.end())
+#define maxvec(v) *max_element(v.begin(), v.end())
+#define mp make_pair
+#define pb(x) push_back(x)
+#define fst first
+#define sec second
+#define foru(i, n, ty, x) for (int i = n; i < ty; i += x)
+#define ford(i, n, ty, x) for (int i = n; i >= ty; i -= x)
+#define forn(i, ty) foru(i, 0, ty, 1)
+
+void setIO(string name) {
+	cin.tie(0)->sync_with_stdio(0);
+	if (name != "") {
+		freopen((name + ".in").c_str(), "r", stdin);
+		freopen((name + ".out").c_str(), "w", stdout);
+	}
+}
+
+void solve() {
+    int n; cin >> n;
+    int o; cin >> o;
+    bool f = true;
+    forn(i, n-1) {
+        int a;
+        cin >> a;
+        if (a % 2 != o % 2) f = false;
+    }
+    cout << (f ? "YES\n" : "NO\n");
+}
+ 
+int32_t main() {
+    setIO("");
+    int t; cin >> t; while(t--)
+    solve();
+}	
