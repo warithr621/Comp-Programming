400c3510d8e93cb4dc2039e94ad82db0bceb8d5b
Warith Rahman <64328893+warithr621@users.noreply.github.com>
2021-07-28 18:28:36 -0500

    Create Raymond.java

---
+++ UIL Computer Science/2017-2018/District/Raymond.java
@@ -0,0 +1,38 @@
+//Problem 12
+
+import java.io.*;
+import java.util.*;
+
+public class Raymond {
+    static BufferedReader br;
+    static PrintWriter out;
+	static StringTokenizer st = new StringTokenizer("");
+	static int T = 1;
+	public static void main(String[] args) throws IOException {
+	   // br = new BufferedReader(new InputStreamReader(System.in));
+	    br = new BufferedReader(new FileReader("raymond.in"));
+		out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+// 		T = nexti();
+		for (int tt = 1; tt <= T; tt++) {
+		    String[] s = br.readLine().split(" ");
+		    for (int i = 0; i < s.length; i++) {
+		        int x = Integer.parseInt(s[i]);
+		        out.printf("%d %d%n", x, ~x);
+		    }
+		}
+		br.close(); out.close();
+	}
+	static String next() throws IOException{
+        while(!st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+        return st.nextToken();
+    }
+    static int nexti() throws IOException {
+        return Integer.parseInt(next());
+    }
+    static long nextl() throws IOException {
+        return Long.parseLong(next());
+    }
+    static double nextd() throws IOException {
+        return Double.parseDouble(next());
+    }
+}
