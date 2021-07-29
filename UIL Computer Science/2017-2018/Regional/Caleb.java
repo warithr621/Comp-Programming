2c9a76335c7c9ec9f88410575ab7f52d96343ab1
Warith Rahman <64328893+warithr621@users.noreply.github.com>
2021-07-29 14:42:48 -0500

    Update Caleb.java

--- UIL Computer Science/2017-2018/Regional/Caleb.java
+++ UIL Computer Science/2017-2018/Regional/Caleb.java
@@ -10,7 +10,7 @@ public class Caleb {
 	static int T = 1;
 	public static void main(String[] args) throws IOException {
 	   // br = new BufferedReader(new InputStreamReader(System.in));
-	    br = new BufferedReader(new FileReader("caleb.in"));
+	    br = new BufferedReader(new FileReader("caleb.dat"));
 		out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
 		T = nexti();
 		for (int tt = 1; tt <= T; tt++) {
