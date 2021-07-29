65b550732b5b715ffd34dc86e567af0c5b24f000
Warith Rahman <64328893+warithr621@users.noreply.github.com>
2021-07-29 15:37:17 -0500

    Create Ankur.java

---
+++ UIL Computer Science/2017-2018/State/Ankur.java
@@ -0,0 +1,34 @@
+//Problem 1
+
+import java.io.*;
+import java.util.*;
+
+public class Ankur {
+    static BufferedReader br;
+    static PrintWriter out;
+	static StringTokenizer st = new StringTokenizer("");
+	static int T = 1;
+	public static void main(String[] args) throws IOException {
+	    br = new BufferedReader(new InputStreamReader(System.in));
+	   // br = new BufferedReader(new FileReader("ankur.dat"));
+		out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+// 		T = nexti();
+		for (int tt = 1; tt <= T; tt++) {
+		    out.println("3141.59\n3141.592654\n3.141593e+03\n271.828\n271.828183\n2.718282e+02");
+		}
+		br.close(); out.close();
+	}
+	static String next() throws IOException{
+        while(!st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+        return st.nextToken();
+    }
+    static int nexti() throws IOException {
+        return Integer.parseInt(next());
+    }
+    static long nextl() throws IOException {
+        return Long.parseLong(next());
+    }
+    static double nextd() throws IOException {
+        return Double.parseDouble(next());
+    }
+}
