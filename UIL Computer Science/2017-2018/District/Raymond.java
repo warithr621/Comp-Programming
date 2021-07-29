a9f59d4a80bf7a6aa55b3ecc9420b9a40ff3741d
Warith Rahman <64328893+warithr621@users.noreply.github.com>
2021-07-29 14:41:58 -0500

    Update Raymond.java

--- UIL Computer Science/2017-2018/District/Raymond.java
+++ UIL Computer Science/2017-2018/District/Raymond.java
@@ -10,7 +10,7 @@ public class Raymond {
 	static int T = 1;
 	public static void main(String[] args) throws IOException {
 	   // br = new BufferedReader(new InputStreamReader(System.in));
-	    br = new BufferedReader(new FileReader("raymond.in"));
+	    br = new BufferedReader(new FileReader("raymond.dat"));
 		out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
 // 		T = nexti();
 		for (int tt = 1; tt <= T; tt++) {
