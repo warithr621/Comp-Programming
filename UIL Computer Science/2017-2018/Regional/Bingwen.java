e03f2054ef2794e0b166b941e103f55d653ebf6b
Warith Rahman <64328893+warithr621@users.noreply.github.com>
2021-07-29 14:42:41 -0500

    Update Bingwen.java

--- UIL Computer Science/2017-2018/Regional/Bingwen.java
+++ UIL Computer Science/2017-2018/Regional/Bingwen.java
@@ -10,7 +10,7 @@ public class Bingwen {
 	static int T = 1;
 	public static void main(String[] args) throws IOException {
 	   // br = new BufferedReader(new InputStreamReader(System.in));
-	    br = new BufferedReader(new FileReader("bingwen.in"));
+	    br = new BufferedReader(new FileReader("bingwen.dat"));
 		out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
 // 		T = nexti();
 		for (int tt = 1; tt <= T; tt++) {
