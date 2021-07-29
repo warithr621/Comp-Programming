28a0a14bc195f72f61e55dc8ecb17aec41defdee
Warith Rahman <64328893+warithr621@users.noreply.github.com>
2021-07-29 14:43:08 -0500

    Update Prashant.java

--- UIL Computer Science/2017-2018/Regional/Prashant.java
+++ UIL Computer Science/2017-2018/Regional/Prashant.java
@@ -10,7 +10,7 @@ public class Prashant {
 	static int T = 1;
 	public static void main(String[] args) throws IOException {
 	   // br = new BufferedReader(new InputStreamReader(System.in));
-	    br = new BufferedReader(new FileReader("prashant.in"));
+	    br = new BufferedReader(new FileReader("prashant.dat"));
 		out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
 // 		T = nexti();
 		for (int tt = 1; tt <= T; tt++) {
