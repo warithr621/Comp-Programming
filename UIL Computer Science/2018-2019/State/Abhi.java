f71c9fe74de29124f5b850d81fe261f738278de6
Warith Rahman <64328893+warithr621@users.noreply.github.com>
2021-07-29 14:45:29 -0500

    Update Abhi.java

--- UIL Computer Science/2018-2019/State/Abhi.java
+++ UIL Computer Science/2018-2019/State/Abhi.java
@@ -10,7 +10,7 @@ public class Abhi {
 	static int T = 1;
 	public static void main(String[] args) throws IOException {
 	    br = new BufferedReader(new InputStreamReader(System.in));
-	   // br = new BufferedReader(new FileReader("abhi.in"));
+	   // br = new BufferedReader(new FileReader("abhi.dat"));
 		out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
 // 		T = nexti();
 		for (int tt = 1; tt <= T; tt++) {
