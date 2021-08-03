021b9cf50eb7976a4811689255ad40bf627e458e
Warith Rahman <64328893+warithr621@users.noreply.github.com>
2021-08-03 13:54:11 -0500

    Create alyona_num.java

---
+++ CodeForces/alyona_num.java
@@ -0,0 +1,37 @@
+//Problem: https://codeforces.com/contest/682/problem/A
+
+import java.io.*;
+import java.util.*;
+
+public class alyona_num {
+    static BufferedReader br;
+    static PrintWriter out;
+	static StringTokenizer st = new StringTokenizer("");
+	static int T = 1;
+	public static void main(String[] args) throws IOException {
+	    br = new BufferedReader(new InputStreamReader(System.in));
+	   // br = new BufferedReader(new FileReader("main.dat"));
+		out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+// 		T = nexti();
+		for (int tt = 1; tt <= T; tt++) {
+		    long n = nextl(), m = nextl();
+		    long sum = 0;
+		    for (int i = 1; i <= n; i++) sum += (m + i%5L) / 5L;
+		    out.println(sum);
+		}
+		br.close(); out.close();
+	}
+	static String next() throws IOException{
+        while(!st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+        return st.nextToken();
+    }
+    static int nexti() throws IOException {
+        return Integer.parseInt(next());
+    }
+    static long nextl() throws IOException {
+        return Long.parseLong(next());
+    }
+    static double nextd() throws IOException {
+        return Double.parseDouble(next());
+    }
+}
