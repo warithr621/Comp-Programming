3e7b3db2ea3a886358c239c84a0e8b8e633ff059
Warith Rahman <64328893+warithr621@users.noreply.github.com>
2021-09-10 15:05:34 -0500

    Create parallelepiped.java

---
+++ Codeforces/parallelepiped.java
@@ -0,0 +1,36 @@
+//Problem: https://codeforces.com/problemset/problem/224/A
+
+import java.io.*;
+import java.util.*;
+
+public class parallelepiped {
+    static BufferedReader br;
+    static PrintWriter out;
+	static StringTokenizer st = new StringTokenizer("");
+	static int T = 1;
+	public static void main(String[] args) throws IOException {
+	    br = new BufferedReader(new InputStreamReader(System.in));
+	   // br = new BufferedReader(new FileReader("main.dat"));
+		out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+// 		T = nexti();
+		for (int tt = 1; tt <= T; tt++) {
+		    int AA = nexti(), BB = nexti(), CC = nexti();
+		    int a = (int)Math.sqrt(AA * BB / CC), b = (int)Math.sqrt(BB * CC / AA), c = (int)Math.sqrt(AA*CC/BB);
+		    out.println(4*(a+b+c));
+		}
+		br.close(); out.close();
+	}
+	static String next() throws IOException{
+        while(!st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+        return st.nextToken();
+    }
+    static int nexti() throws IOException {
+        return Integer.parseInt(next());
+    }
+    static long nextl() throws IOException {
+        return Long.parseLong(next());
+    }
+    static double nextd() throws IOException {
+        return Double.parseDouble(next());
+    }
+}
