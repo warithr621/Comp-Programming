5587bf69c3fa0946f0c3d4bdb8cc2f2839f221d7
Warith Rahman <64328893+warithr621@users.noreply.github.com>
2022-06-29 18:41:14 -0500

    CSES Creating Strings II

---
+++ USACO_and_Guide/create_string_ii.cpp
@@ -0,0 +1,49 @@
+// CSES Creating Strings II
+
+#include <iostream>
+#include <vector>
+#define int long long
+#define MOD 1000000007
+using namespace std;
+
+int fact[1000001], inv[1000001];
+
+int pow(int base, int exp, int m) {
+	if (base == 0) return (exp == 0 ? 1 : 0);
+	if (base == 1) return 1;
+	base %= m;
+ 
+	int ans = 1;
+	while (exp > 0) {
+	    if (exp % 2 == 1) ans = ans * base % m;
+	    base = base * base % m;
+	    exp >>= 1;
+	}
+	
+	return ans;
+}
+
+void pre_fact() {
+	fact[0] = 1;
+	for (int i = 1; i <= 1000000; i++) fact[i] = fact[i-1] * i % MOD;
+}
+
+void pre_inv() {
+	inv[0] = 1;
+	for (int i = 1; i <= 1000000; i++) inv[i] = pow(fact[i], MOD-2, MOD);
+}
+
+void solve() {
+	string s; cin >> s;
+	vector<int> cnt(26, 0);
+	for (int i = 0; i < (int) s.size(); i++) cnt[s[i] - 'a']++;
+	int ans = fact[(int) s.size()];
+	for (int i : cnt) if (i != 1) ans = ans * inv[i] % MOD;
+	cout << ans << '\n';
+}
+
+int32_t main() {
+	cin.tie(0)->sync_with_stdio(0);
+	pre_fact(); pre_inv(); //precomputations
+	solve();
+}
